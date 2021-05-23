b75504050c0a8933c21de5442d5a39be8e863255
Matvey <r3dw0r13@yandex.ru>
2021-05-24 00:51:12 +0400

    signIn footer

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -68,6 +68,29 @@ const useStyles = makeStyles((theme) => ({
     marginTop: '20px',
     marginBottom: '45px',
   },
+  footer: {
+    height: '84px',
+    display: 'flex',
+    alignItems: 'center',
+    '& ul': {
+      listStyle: 'none',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+      '& li': {
+        fontSize: 13,
+        margin: '0 10px',
+        '& a': {
+          textDecoration: 'none',
+          color: 'black',
+          '&:hover': {
+            textDecoration: 'underline  ',
+          },
+        },
+      },
+    },
+  },
 }));
 
 // type PropsType = {};
@@ -80,119 +103,177 @@ export const SignIn: React.FC = (props) => {
   const handleCloseModal = (): void => setVisibleModal(undefined);
 
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.blueBlock}>
-        <TwitterIcon color="primary" className={classes.blueBlockIcon} />
-        <ul className={classes.blueBlockListInfo}>
-          <li className={classes.blueBlockListInfoItem}>
-            <Typography variant="h6">
-              <SearchIcon className={classes.blueBlockListInfoIcon} />
-              Читайте о том, что вам интересно.
-            </Typography>
-          </li>
-          <li className={classes.blueBlockListInfoItem}>
-            <Typography variant="h6">
-              <PeopleOutlineIcon className={classes.blueBlockListInfoIcon} />
-              Узнайте, о чем говорят в мире.
-            </Typography>
-          </li>
-          <li className={classes.blueBlockListInfoItem}>
-            <Typography variant="h6">
-              <ChatBubbleOutlineIcon className={classes.blueBlockListInfoIcon} />
-              Присоединяйтесь к общению.
-            </Typography>
-          </li>
-        </ul>
-      </div>
-      <div className={classes.loginBlock}>
-        <div className={classes.loginBlockWrapper}>
-          <TwitterIcon color="primary" className={classes.loginBlockTwitterItem} />
-          <Typography className={classes.loginSideTitle} variant="h4">
-            Узнайте, что происходит в мире прямо сейчас
-          </Typography>
-          <Typography style={{ marginBottom: 20 }}>
-            <b>Присоединяйтесь к Твиттеру прямо сейчас!</b>
-          </Typography>
-          <Button
-            onClick={signUpOpen}
-            style={{ marginBottom: 20 }}
-            variant="contained"
-            color="primary"
-            fullWidth>
-            Зарегестрироватся
-          </Button>
-          <Button onClick={signInOpen} variant="outlined" color="primary" fullWidth>
-            Войти
-          </Button>
+    <>
+      <div className={classes.wrapper}>
+        <div className={classes.blueBlock}>
+          <TwitterIcon color="primary" className={classes.blueBlockIcon} />
+          <ul className={classes.blueBlockListInfo}>
+            <li className={classes.blueBlockListInfoItem}>
+              <Typography variant="h6">
+                <SearchIcon className={classes.blueBlockListInfoIcon} />
+                Читайте о том, что вам интересно.
+              </Typography>
+            </li>
+            <li className={classes.blueBlockListInfoItem}>
+              <Typography variant="h6">
+                <PeopleOutlineIcon className={classes.blueBlockListInfoIcon} />
+                Узнайте, о чем говорят в мире.
+              </Typography>
+            </li>
+            <li className={classes.blueBlockListInfoItem}>
+              <Typography variant="h6">
+                <ChatBubbleOutlineIcon className={classes.blueBlockListInfoIcon} />
+                Присоединяйтесь к общению.
+              </Typography>
+            </li>
+          </ul>
         </div>
+        <div className={classes.loginBlock}>
+          <div className={classes.loginBlockWrapper}>
+            <TwitterIcon color="primary" className={classes.loginBlockTwitterItem} />
+            <Typography className={classes.loginSideTitle} variant="h4">
+              Узнайте, что происходит в мире прямо сейчас
+            </Typography>
+            <Typography style={{ marginBottom: 20 }}>
+              <b>Присоединяйтесь к Твиттеру прямо сейчас!</b>
+            </Typography>
+            <Button
+              onClick={signUpOpen}
+              style={{ marginBottom: 20 }}
+              variant="contained"
+              color="primary"
+              fullWidth>
+              Зарегестрироватся
+            </Button>
+            <Button onClick={signInOpen} variant="outlined" color="primary" fullWidth>
+              Войти
+            </Button>
+          </div>
+        </div>
+        <ModalUI
+          title="Войти в Twitter"
+          visible={visibleModal === 'signIn'}
+          onClose={handleCloseModal}>
+          <FormControl fullWidth>
+            <FormGroup row>
+              <TextField
+                id="email"
+                type="text"
+                label="E-Mail"
+                margin="normal"
+                variant="filled"
+                autoFocus
+                fullWidth
+              />
+
+              <TextField
+                id="password"
+                type="password"
+                label="Password"
+                margin="normal"
+                variant="filled"
+                autoFocus
+                fullWidth
+              />
+            </FormGroup>
+          </FormControl>
+        </ModalUI>
+        <ModalUI
+          title="Создайте учетуню записать"
+          visible={visibleModal === 'signUp'}
+          onClose={handleCloseModal}>
+          <FormControl fullWidth>
+            <FormGroup row>
+              <TextField
+                id="name"
+                type="text"
+                label="Name"
+                margin="normal"
+                variant="filled"
+                autoFocus
+                fullWidth
+              />
+
+              <TextField
+                id="email"
+                type="text"
+                label="E-Mail"
+                margin="normal"
+                variant="filled"
+                autoFocus
+                fullWidth
+              />
+
+              <TextField
+                id="password"
+                type="password"
+                label="Password"
+                margin="normal"
+                variant="filled"
+                autoFocus
+                fullWidth
+              />
+            </FormGroup>
+          </FormControl>
+        </ModalUI>
       </div>
-      <ModalUI
-        title="Войти в Twitter"
-        visible={visibleModal === 'signIn'}
-        onClose={handleCloseModal}>
-        <FormControl fullWidth>
-          <FormGroup row>
-            <TextField
-              id="email"
-              type="text"
-              label="E-Mail"
-              margin="normal"
-              variant="filled"
-              autoFocus
-              fullWidth
-            />
-
-            <TextField
-              id="password"
-              type="password"
-              label="Password"
-              margin="normal"
-              variant="filled"
-              autoFocus
-              fullWidth
-            />
-          </FormGroup>
-        </FormControl>
-      </ModalUI>
-
-      <ModalUI
-        title="Создайте учетуню записать"
-        visible={visibleModal === 'signUp'}
-        onClose={handleCloseModal}>
-        <FormControl fullWidth>
-          <FormGroup row>
-            <TextField
-              id="name"
-              type="text"
-              label="Name"
-              margin="normal"
-              variant="filled"
-              autoFocus
-              fullWidth
-            />
-
-            <TextField
-              id="email"
-              type="text"
-              label="E-Mail"
-              margin="normal"
-              variant="filled"
-              autoFocus
-              fullWidth
-            />
-
-            <TextField
-              id="password"
-              type="password"
-              label="Password"
-              margin="normal"
-              variant="filled"
-              autoFocus
-              fullWidth
-            />
-          </FormGroup>
-        </FormControl>
-      </ModalUI>
-    </div>
+      <footer className={classes.footer}>
+        <ul>
+          <li>
+            <a href="https://kmudze.netlify.app/">О нас</a>
+          </li>
+          <li>
+            <a href="https://help.twitter.com/en">Справочный центр</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/en/tos">Условия предоставления услуг</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/en/privacy">Политика конфиденциальности</a>
+          </li>
+          <li>
+            <a href="https://help.twitter.com/en/rules-and-policies/twitter-cookies">
+              Политика в отношении файлов cookie
+            </a>
+          </li>
+          <li>
+            <a href="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html">
+              Информация о рекламе
+            </a>
+          </li>
+          <li>
+            <a href="https://blog.twitter.com/">Блог</a>
+          </li>
+          <li>
+            <a href="https://status.twitterstat.us/">Статус</a>
+          </li>
+          <li>
+            <a href="https://careers.twitter.com/">Работа</a>
+          </li>
+          <li>
+            <a href="https://about.twitter.com/press/brand-assets">Ресурсы бренда</a>
+          </li>
+          <li>
+            <a href="https://ads.twitter.com/login?ref=gl-tw-tw-twitter-advertise">Реклама</a>
+          </li>
+          <li>
+            <a href="https://marketing.twitter.com/en">Маркетинг</a>
+          </li>
+          <li>
+            <a href="https://business.twitter.com/en/g-2.html">Твиттер для бизнеса</a>
+          </li>
+          <li>
+            <a href="https://developer.twitter.com/en">Разработчикам</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/i/directory/profiles">Каталог</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/settings/account/personalization">Настройки</a>
+          </li>
+          <li>© Twitter, Inc., 2021.</li>
+        </ul>
+      </footer>
+    </>
   );
 };
